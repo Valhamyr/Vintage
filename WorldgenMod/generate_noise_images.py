91ded7cec3eea741b2e83c297c853e46f367b659
Valhamyr <sapje16@hotmail.com>
2025-08-12 20:20:45 +0200

    Add new step mountain landforms

--- WorldgenMod/generate_noise_images.py
+++ WorldgenMod/generate_noise_images.py
@@ -103,6 +103,12 @@ else:
         "step_mountains_6tier_test_large_123",
         "step_mountains_6tier_test_largemid_456",
         "step_mountains_6tier_test_smallall",
+        "step_mountains_6tier_test_4all",
+        "step_mountains_6tier_med2",
+        "step_mountains_6tier_test_4_123",
+        "step_mountains_6tier_test_4_456",
+        "step_mountains_6tier_test_4_789",
+        "step_mountains_6tier_test_4_23_2_5_2_7",
     }
     landforms = [lf for lf in landforms if lf.get("code") in allowed_codes]
 
